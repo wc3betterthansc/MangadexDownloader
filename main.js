89da28a3f14bb111b73147f9c29a009c9ec7c8ec
wc3betterthansc <>
2020-05-12 10:28:23 -0400

    changed package.json

--- main.js
+++
@@ -1,9 +0,0 @@
-const MangadexDownloader = require('../mangadex/mangadex');
-
-const mangadexDownloader = new MangadexDownloader(32775, {
-  dir: "E:\\Mango\\Scanlated\\Fechippuru ~Our Innocent Love~",
-  firstChapter: 40,
-  group: 8802
-});
-
-mangadexDownloader.download();
