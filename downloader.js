2039bd5a11ca3d388e919fdee8f19dc9f1c3c955
wc3betterthansc <>
2020-07-11 03:58:54 -0400

    added downloader.js, uses argparse

---
+++ downloader.js
@@ -0,0 +1,78 @@
+const ArgumentParser = require("argparse").ArgumentParser;
+const downloaders = require("./mangadex-downloader");
+
+const parser = new ArgumentParser({
+    version: "1.0.0",
+    addHelp: true,
+    description: "Download manga from mangadex."
+});
+
+parser.addArgument(
+    ["-i","--id"],
+    {
+        help: "The id of the manga",
+        type: "int",
+        required: true,
+    }
+);
+
+parser.addArgument(
+    ["-d","--dir"],
+    {
+        help: "The directory where the manga chapters will be downloaded.",
+        type: "string",
+        required: false
+    }
+);
+
+parser.addArgument(
+    ["-vb","--verbose"],
+    {
+        help: "Feedback from the downloader.",
+        type: Boolean,
+        action: "storeTrue"
+    }
+);
+
+parser.addArgument(
+    ["-a","--autoUpdate"],
+    {
+        help: "If this option is selected the manga will be automatically updated.",
+        type: Boolean,
+        action: "storeTrue"
+    }
+);
+
+parser.addArgument(
+    ["-f","--firstChapter"],
+    {
+        help: "The first chapter to download.",
+        type: "int",
+        required: false,
+    }
+);
+
+parser.addArgument(
+    ["-l","--lastChapter"],
+    {
+        help: "The last chapter to download.",
+        type: "int",
+        required: false,
+    }
+);
+
+const args = parser.parseArgs();
+const {id,dir,verbose,autoUpdate,firstChapter,lastChapter} = args;
+let downloaderClass;
+
+if(verbose) {
+    if(autoUpdate) downloaderClass = downloaders.VerboseManualMangadexDownloader;
+    else downloaderClass = downloaders.VerboseMangadexDownloader;
+}
+else {
+    if(autoUpdate) downloaderClass = downloaders.ManualMangadexDownloader;
+    else downloaderClass = downloaders.MangadexDownloader;
+}
+
+const {download} = downloaderClass;
+download(id,{dir})
